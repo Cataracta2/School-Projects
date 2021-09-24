c32639715fa09235000086c667e947a15ec9aae4
YazeedKHasan <91287440+YazeedKHasan@users.noreply.github.com>
2021-09-24 06:57:52 -0400

    Add files via upload

---
+++ GregCalendar.java
@@ -0,0 +1,13 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+public class GregCalendar {
+    public static void main(String[] args) {
+        GregorianCalendar cal = new GregorianCalendar()
+        GregorianCalendar myBirthday = new GregorianCalendar(2005, Calendar.JANUARY, 6);
+
+        int Weekday = cal.get(Calendar.DAY_OF_WEEK);
+        int HundredDay
+        System.out.println("1 is Sunday, 2 is Monday, 3 is Tuesday, 4 is Wednesday, 5 is Thursday, 6 is Friday, 7 is Saturday");
+
+    }
+}
