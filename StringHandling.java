cebd60c8682f1bb5844be3532f7a8ec88fc92aef
YazeedKHasan <91287440+YazeedKHasan@users.noreply.github.com>
2021-10-18 16:47:11 -0400

    Add files via upload

---
+++ StringHandling.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class StringHandling {
+    public static void main(String[] args) {
+        System.out.println("Input any word or sentence, press enter afterwards");
+        Scanner in = new Scanner(System.in);
+        String input1 = in.nextLine();
+        System.out.println("_____________________________________________");
+        System.out.println("Your Original Sentence: " + input1);
+        System.out.println("String Length: " + input1.length());
+        System.out.println("First index of a space: " + input1.indexOf(" "));
+        System.out.println("The sentence after the third space: " + );
+
+    }
+}
+
+
+
