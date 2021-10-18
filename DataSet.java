9fb35af4dac0f4617cd3ccc63d901607361f6273
YazeedKHasan <91287440+YazeedKHasan@users.noreply.github.com>
2021-10-18 16:40:35 -0400

    Add files via upload

--- DataSet.java
+++ DataSet.java
@@ -1,16 +1,35 @@
 public class DataSet {
-    private int num1;
-    private int num2;
-    private int num3;
-    private int num4;
-    
-    public DataSet(double input1, double input2, double input3, double input4){
-        input1 = num1;
-        input2 = num2;
-        input3 = num3;
-        input4 = num4;
+    private double num1;
+    private double num2;
+    private double num3;
+    private double num4;
+    private int initialMax;
+    private int initialMin;
+
+    public DataSet(double input1, double input2, double input3, double input4) {
+        num1 = input1;
+        num2 = input2;
+        num3 = input3;
+        num4 = input4;
     }
-    public double Max(){
+
+    public double Max() {
+        double max1 = Math.max(num1, num2);
+        double max2 = Math.max(num3, num4);
+        return Math.max(max1, max2);
 
     }
+
+    public double Min() {
+        double min1 = Math.min(num1, num2);
+        double min2 = Math.min(num3, num4);
+        return Math.min(min1, min2);
+    }
+    public DataSet(){
+        initialMax = Integer.MAX_VALUE;
+        initialMin = Integer.MIN_VALUE;
+    }
 }
+
+
+
--- DataSetTester.java
+++ DataSetTester.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 public class DataSetTester {
     public static void main(String[] args) {
         System.out.println("Input any 4 numbers, after each number click enter");
+        Scanner in = new Scanner(System.in);
+        double num1 = in.nextInt();
+        double num2 = in.nextInt();
+        double num3 = in.nextInt();
+        double num4 = in.nextInt();
+        double Max;
+        double Min;
+        DataSet answer = new DataSet(num1, num2, num3, num4);
+        Max = answer.InitialMax();
+        Min = answer.InitialMin();
+        Max = answer.Max();
+        Min = answer.Min();
+        System.out.println(Max);
+        System.out.println(Min);
+
 
 
     }
